5c99ac18f722ad1036834e1bb9b86b94eac7e9c7
tannerkrewson <tannerkrewson@gmail.com>
2018-02-24 16:36:07 -0600

    Added a simple testing main

--- CS470_HAC/src/main/Main.java
+++ CS470_HAC/src/main/Main.java
@@ -3,8 +3,24 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!!");
+		System.out.println("Welcome to the Spice gUrls HAC!");
+		while (true) {
+			System.out.println("Enter a mode:");
+			System.out.println("1 - Inital Node");
+			System.out.println("2 - Connect to a node");
+			Scanner sc = new Scanner(System.in);
+			int i = sc.nextInt();
+			if (i == 1) {
+				Listener testListener = new Listener();
+				testListener.createAndListenSocket();
+			} else if (i == 2) {
+				Messenger testMessenger = new Messenger();
+				testMessenger.createAndListenSocket();
+			}
 
+		}
+		
+		
 	}
 
 }
