f5ac65d31846dac0c648d789ecfc66569eb0219c
Brandon Heisserer <brandonheisserer@gmail.com>
2018-03-05 21:31:25 -0600

    Made changes to correspond with changes to heartbeat buffer
    changed variable name, function call, and removed commented out code

--- CS470_HAC/src/edu/truman/spicegURLs/node/Messenger.java
+++ CS470_HAC/src/edu/truman/spicegURLs/node/Messenger.java
@@ -32,10 +32,10 @@ public class Messenger {
 			  @Override
 			  public void run() {
 				  // pull info from heartbeat buffer
-				  String changes = hbb.getChanges();
+				  String packet = hbb.getPacket();
 				  
 				  // send data to all peers on list
-				  Messenger.this.sendChangesToAll(changes);
+				  Messenger.this.sendChangesToAll(packet);
 				  
 				  // wait again
 				  Messenger.this.waitToSendNextHeartbeat();
@@ -65,35 +65,4 @@ public class Messenger {
 			e.printStackTrace();
 		}
 	}
-	
-    /*public void createAndListenSocket() 
-    {
-        try 
-        {
-            InetAddress IPAddress = InetAddress.getByName("150.243.147.215");
-            byte[] incomingData = new byte[1024];
-            String sentence = "Tell me what you want";
-            byte[] data = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, 9876);
-            socket.send(sendPacket);
-            System.out.println("Message sent from client");
-            DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-            socket.receive(incomingPacket);
-            String response = new String(incomingPacket.getData());
-            System.out.println("Response from server:" + response);
-            socket.close();
-        }
-        catch (UnknownHostException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (SocketException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
-    }*/
 }
