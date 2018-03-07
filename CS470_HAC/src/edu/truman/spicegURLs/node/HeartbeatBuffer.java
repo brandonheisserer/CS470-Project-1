faf658025ca64d7bf8d568dc1545ecf0b676ff34
Brandon Heisserer <brandonheisserer@gmail.com>
2018-03-06 19:49:27 -0600

    Removed join flag code and added documentation for HeartbeatBuffer

--- CS470_HAC/src/edu/truman/spicegURLs/node/HeartbeatBuffer.java
+++ CS470_HAC/src/edu/truman/spicegURLs/node/HeartbeatBuffer.java
@@ -3,6 +3,15 @@ package edu.truman.spicegURLs.node;
 import java.net.InetAddress;
 import java.util.ArrayList;
 
+/**
+ * Keeps track of changes to the up nodes and down nodes in the
+ * system and reports them when asked.
+ * @author Brandon Crane
+ * @author Brandon Heisserer
+ * @author Tanner Krewson
+ * @author Carl Yarwood
+ * @version 6 March 2018
+ */
 public class HeartbeatBuffer {
 	
 	public static final String version = "1.0";
@@ -11,20 +20,38 @@ public class HeartbeatBuffer {
 	private ArrayList<InetAddress> upList;
 	private ArrayList<InetAddress> downList;
 	
+	/**
+	 * Creates an instance of the object HeartbeatBuffer and initializes
+	 * all local variables.
+	 * @return HeartbeatBuffer object.
+	 */
 	public HeartbeatBuffer () {
 		join = true;
 		upList = new ArrayList<>();
 		downList = new ArrayList<>();
 	}
 	
+	/**
+	 * Adds the given address to the list of up nodes.
+	 * @param upNode the IP address to add to upList
+	 */
 	public void addToUpList (InetAddress upNode) {
 		upList.add(upNode);
 	}
 	
+	/**
+	 * Adds the given address to the list of down nodes.
+	 * @param downNode the IP address to add to downList
+	 */
 	public void addToDownList (InetAddress downNode) {
 		downList.add(downNode);
 	}
 	
+	/**
+	 * Creates and returns a string representation of packet including
+	 * the lists, version, and whether or not this is a join packet.
+	 * @return
+	 */
 	public String getPacket() {
 		String packet = version + ";";
 		if (join) {
@@ -46,6 +73,9 @@ public class HeartbeatBuffer {
 		return packet;
 	}
 	
+	/**
+	 * Removes everything from both upList and downList.
+	 */
 	private void emptyLists() {
 		upList = new ArrayList<>();
 		downList = new ArrayList<>();
